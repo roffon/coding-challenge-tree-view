5d03dff588731498d6ef3b038d8e98dd08913ce4
Agud Ramon <67280933+ZenAl3x@users.noreply.github.com>
2024-09-11 15:13:19 +0300

    Challenge view and canvas finished

--- src/components/CanvasComp.vue
+++ src/components/CanvasComp.vue
@@ -1,11 +1,15 @@
 <template>
     <div class="canvas-outer">
-        asd
+        <individual-tree-object-comp v-for="( item, i ) in shownObjects" :key="i" :data="item" />
     </div>
 </template>
 
 <script setup>
+import {ref} from "vue";
+import objects from "../assets/objects.json"
+import IndividualTreeObjectComp from "@/components/IndividualTreeObjectComp.vue";
 
+const shownObjects = ref(objects)
 </script>
 
 <style scoped lang="scss">
@@ -15,5 +19,8 @@
     border-radius: 4px;
     padding: 2rem;
     margin-top: 4rem;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 2rem;
 }
 </style>
---
+++ src/components/IndividualTreeObjectComp.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-sheet width="220" class="text-center px-3 py-4 rounded-lg" color="#4A4A4A">
+        <h3 class="font-weight-bold">{{ data.name }}</h3>
+    </v-sheet>
+</template>
+
+<script setup>
+const props = defineProps({
+    data: Object,
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>
