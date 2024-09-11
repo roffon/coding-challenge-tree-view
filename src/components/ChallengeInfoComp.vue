54acd5a551ff03cd834f6c45e8f28fc1cdd388f3
Agud Ramon <67280933+ZenAl3x@users.noreply.github.com>
2024-09-11 14:46:22 +0300

    Added challenge info component

---
+++ src/components/ChallengeInfoComp.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="challenge-info-outer">
+        <h1 class="title d-flex align-center justify-center ga-3">
+            <v-icon>mdi-information-outline</v-icon>
+            Challenge info
+        </h1>
+        
+        <h3 class="mt-8">
+            In this challenge you will need to build a <span class="highlited">responsive tree-like</span> structure using
+            recursivity and advanced <span class="vue-color">Vue 3</span> principles. Completing this assignment will prove your
+            Javascript knowledge and your proficiency using web development tools and technologies.
+        </h3>
+        
+        <h2 class="text-left highlited mt-6">Here are all the details:</h2>
+        <ul class="text-left">
+            <li>
+                You will be provided with a standard JSON array of objects dynamically rendered
+                using the v-for directive. Each component contains a property called
+                <span class="highlited">parent_id</span>, among others. You will need to build a tree from
+                parents to children contained in a <i>(hint incoming)</i> recursive component based on the
+                object's <span class="highlited">parent_id</span> property.
+            </li>
+            <li>
+                Your tree view component <b>needs</b> to be responsive and work seamlessly if more
+                objects are added to the array. <i>(You don't need to create an addition function)</i>
+            </li>
+            <li>
+                Your component should be somewhat efficient and the rendering as fast as possible.
+            </li>
+            <li>
+                You will be provided with the individual vue components needed to display the objects
+            </li>
+            <li>
+                To start working on the challenge go ahead and <v-icon>mdi-silverware-fork</v-icon> fork
+                this repository and click the <i>totally not too big</i> button below. You should have
+                everything you need inside the project. <i>Tip: Work inside the component named <b>CanvasComp</b></i>
+            </li>
+        </ul>
+        
+        <v-btn color="#60b083" height="60" variant="outlined">
+            <div>
+                <h3>Bring it on!</h3>
+                <p>Totally a normal sized button</p>
+            </div>
+        </v-btn>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped lang="scss">
+.challenge-info-outer {
+    width: 100%;
+    text-align: center;
+    border: 2px solid grey;
+    border-radius: 4px;
+    padding: 2rem;
+    h1 {
+        .v-icon {
+            font-size: 2rem;
+            color: white;
+        }
+    }
+    
+    ul {
+        padding: 12px 12px 12px 100px;
+    }
+    
+    .v-btn {
+        width: 100%;
+        margin-top: 2rem;
+        h3 {
+            font-weight: bold;
+        }
+        
+        p {
+            font-size: 0.5rem;
+            color: #e0e0e0;
+        }
+    }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="main-outer">
         <welcome-comp />
+        <challenge-info-comp />
     </div>
 </template>
 
 <script setup>
 
 import WelcomeComp from "@/components/WelcomeComp.vue";
+import ChallengeInfoComp from "@/components/ChallengeInfoComp.vue";
 </script>
 
 <style scoped lang="scss">
