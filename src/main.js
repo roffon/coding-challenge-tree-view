b5aecbc5da104680b461f2c3a5ec43278ccc2390
Agud Ramon <67280933+ZenAl3x@users.noreply.github.com>
2024-09-11 13:51:30 +0300

    Added mdi iconset

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 // Comps
 import { createApp } from 'vue'
@@ -12,6 +13,13 @@ import App from './App.vue'
 import router from './router'
 
 const vuetify = createVuetify({
+	icons: {
+		defaultSet: 'mdi',
+		aliases,
+		sets: {
+			mdi,
+		},
+	},
 	components,
 	directives,
 })
